40fc0854369f01c15c3d7287c0a293396238f85e
hparmantier <hector.parmantier@epfl.ch>
2016-10-20 17:39:46 +0200

    init folders

---
+++ clustering/communityDetection.py
@@ -0,0 +1,13 @@
+import numpy as np
+import community
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+recc_matrix = np.load('/home/hparmantier/Montreux Analytics/clustering/data/R_creep.npy')
+G = nx.from_numpy_matrix(recc_matrix)
+part = community.best_partition(G)
+print(part)
+# values = [part.get(node) for  node in G.nodes()]
+# nx.draw_spring(G, map = plt.get_cmap('jet'), node_color= values, node_size=30, with_labels=False)
+# plt.show()
