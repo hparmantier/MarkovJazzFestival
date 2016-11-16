471559f271b6e4b5d40b5f36c95ca3600ae3b3bf
hparmantier <hector.parmantier@epfl.ch>
2016-11-16 16:48:21 +0100

    reset

--- Neo4jInterface/init.py
+++ Neo4jInterface/init.py
@@ -1,6 +1,8 @@
-import edit_db as edb
+import write_db as writer
+import read_db as reader
 
-file = '/home/hparmantier/Montreux Analytics/Scripts/data/creep_graph.gpickle'
-nx = edb.load_nx(file)
-graph_db = edb.connect_to_db()
-edb.intra_neo_from_nx(nx,graph_db)
+
+file = '/home/hparmantier/Montreux Analytics/Project_Git/MarkovJazzFestival/Data/creep_mat.gpickle'
+nx = writer.load_nx(file)
+graph_db = writer.connect_to_db()
+writer.intra_neo_from_nx(nx,graph_db)
