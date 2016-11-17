48046113e5af65f634b5567a32f5b773865dac3e
hparmantier <hector.parmantier@epfl.ch>
2016-11-17 15:00:06 +0100

    path generation, ok. started simulation audio..

--- RandomGraph/RandomStep.py
+++ RandomGraph/RandomStep.py
@@ -1,22 +1,49 @@
 from numpy.random import choice
 import networkx as nx
 
+p_file = '/home/hparmantier/Montreux Analytics/Media/distr_choice.txt'
+f = open(p_file, 'w')
+
+##TODO remove write in file
 def make_step(G, current):
-    edges = filter(lambda e: e[0]==current, G.edges(current, data=True))
+    print(current)
+    edges = list(filter(lambda e: e[0]==current, G.edges(current, data=True)))
+    #print(list(edges))
     neighbors = [e[1] for e in edges]
     pr = [e[2]['weight'] for e in edges]
+    print("current:")
     print(current)
+    print("neighbors:")
     print(neighbors)
+    print("distribution:")
     print(pr)
+    f.write("current:\n")
+    f.write(str(current)+"\n")
+    f.write("neighbors:\n")
+    f.write(','.join(map(lambda i: str(i),neighbors))+"\n")
+    f.write("distribution:\n")
+    f.write(','.join(map(lambda i: str(i),pr))+"\n")
     last_node = len(G)-1
-    chosen = choice(neighbors, 1, pr)
-    return 0 if chosen == last_node else  int(chosen + 1) ##temporal neighbor of node chosen TODO: if last temporal node return 0 (first beat)
+    str_ngb = list(map(lambda n: str(n), neighbors))
+    chosen = int(choice(str_ngb, 1, pr)[0])
+    #chosen = choice(neighbors, 1, pr)
+    print("chosen:")
+    print(chosen)
+    f.write("chosen:\n")
+    f.write(str(chosen)+"\n")
+
+    #toint = int(chosen[0])
+    #print(toint)
+    return 0 if chosen == last_node else  int(chosen + 1)
 
 def make_n_step(G, current, n):
     path = []
     curr = current
     for i in range(n):
         nxt = make_step(G, curr)
+        print("next:")
+        print(nxt)
         path.append(nxt)
         curr = nxt
+    f.close()                                       ##to remove
     return path
--- RandomGraph/runSim.py
+++ RandomGraph/runSim.py
@@ -6,13 +6,23 @@ import write_db as writer
 import read_db as reader
 import SimulateRW as simulator
 
-file = '/home/hparmantier/Montreux Analytics/Scripts/Data/creep_mat.gpickle'
+file = '/home/hparmantier/Montreux Analytics/Scripts/Data/creep_matrix.gpickle'
 nx = writer.load_nx(file)
 p_file = '/home/hparmantier/Montreux Analytics/Media/path.txt'
 
+# for e in nx.edges(data=True):
+#     print(e[2]['weight'])
+
 #reader.cool_draw(nx) #-> draw for testing purpose
 
+
 path = simulator.generate_permutation_nx(nx)
+print(path)
 print("Path Generated")
 simulator.print_path(path, p_file)
 print("Path printed")
+
+beat_nb = len(nx)
+music = "/home/hparmantier/Montreux Analytics/Scripts/Data/creep.mp3"
+audio = simulator.play_permutation(music, path, beat_nb)
+print(audio)
